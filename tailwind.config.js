07baabc62ee7db2f7eee671645008e8f9c14092d
Dimitriy White <oblivvion333@gmail.com>
2023-07-17 23:29:28 +0300

    tailwindcss was added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,vue}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'placeholder-gray': '#f1f0ee',
+      'placeholder-text': '#a1a09e'
+    }
+  },
+  plugins: [],
+}
